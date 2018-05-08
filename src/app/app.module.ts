fa9eecd2d46c54b1f28f461ae6ed745038f205a7
BarinDwalin <sergeishipwright@mail.ru>
2018-05-08 23:12:28 +0300

    Экспорт компонент вынесен в баррель

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,13 +3,15 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
-import { CardHeroComponent } from './components/card-hero/card-hero.component';
-import { GameMenuComponent } from './components/game-menu/game-menu.component';
-import { HeroComponent } from './components/hero/hero.component';
-import { MapComponent } from './components/map/map.component';
-import { MapSettingsComponent } from './components/map-settings/map-settings.component';
-import { StatisticComponent } from './components/statistic/statistic.component';
-import { TreeComponent } from './components/tree/tree.component';
+import {
+  CardHeroComponent,
+  GameMenuComponent,
+  HeroComponent,
+  MapComponent,
+  MapSettingsComponent,
+  StatisticComponent,
+  TreeComponent,
+} from './components';
 
 import { MapService } from './shared/services';
 
---
+++ src/app/components/index.ts
@@ -0,0 +1,7 @@
+export * from './card-hero/card-hero.component';
+export * from './game-menu/game-menu.component';
+export * from './hero/hero.component';
+export * from './map/map.component';
+export * from './map-settings/map-settings.component';
+export * from './statistic/statistic.component';
+export * from './tree/tree.component';
