72b5de3d3e16e3d1f4642845731615359e3bbdbd
BarinDwalin <sergeishipwright@mail.ru>
2018-06-14 22:50:06 +0300

    Исправлены настройки e2e

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -7,8 +7,8 @@ describe('iiengine App', () => {
     page = new AppPage();
   });
 
-  it('should display welcome message', () => {
+  it('should display project name', () => {
     page.navigateTo();
-    expect(page.getParagraphText()).toEqual('Welcome to app!');
+    expect(page.getParagraphText()).toEqual('Собиратели яблок');
   });
 });
